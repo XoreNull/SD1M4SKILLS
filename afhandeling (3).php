fe989b3a77af416da84773b15b59b6b1833ba48a
X0r3 <114577469+XoreNull@users.noreply.github.com>
2023-06-01 14:39:05 +0200

    afhandeling opdracht

---
+++ afhandeling (3).php
@@ -0,0 +1,10 @@
+<?php
+    echo 'welkom op mijn website!!!';
+    var_dump($_GET);
+
+    $achternaam['ken'] = "koorn";
+    $achternaam['awes'] = "zoretic";
+    $achternaam['robert'] = "van rijs";
+
+    echo "robert is" . $achternaam['robert'] .
+?>
